05826b7fa1449dc2365cdaef797ffb1fd3249411
sabrinadeabreu <abreu_sabrina@hotmail.com>
2023-07-23 15:25:12 -0300

    finalizando quadribol

---
+++ quadribol.js
@@ -0,0 +1,6 @@
+const myCarouselElement = document.getElementById('carouselExampleIndicators')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})  
